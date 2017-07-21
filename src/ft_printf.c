ac73eacad1249bca6f251e112a4671e8bc7625e2
Brendan Pierce <bpierce@e1z2r9p48.42.us.org>
2017-07-20 19:26:51 -0700

    Removed // comments

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bpierce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 15:39:32 by bpierce           #+#    #+#             */
-/*   Updated: 2017/07/20 18:47:44 by bpierce          ###   ########.fr       */
+/*   Updated: 2017/07/20 19:26:32 by bpierce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,39 +28,13 @@ int		ft_printf(char *s, ...)
 		if (s[i] == '%' && s[i + 1] == '%')
 			i = (storechar_printlst(&p1, s[i], i) != -1 ? i + 2 : -1);
 		else if (s[i] == '%')
-		{
 			i = store_percentid(&p1, &s[i], &ap, i);
-			//ft_putnbr(p1->type);
-			//ft_putchar(' ');
-			//ft_putnbr(((int)p1->print_func));
-			//ft_putchar(' ');
-			//ft_putnbr(i);
-			//ft_putchar('\n');
-		}
 		else
 			i = storechar_printlst(&p1, s[i], i);
-		if (i == -1) // Remember to print error messages.
-			return (-1); // Try to free memory before returning -1.
+		if (i == -1)
+			return (-1);
 	}
 	va_end(ap);
 	i = print_printlst(&p2);
-	//SOMETHING_TO_FREE_MEMORY(&p2);
 	return (i);
 }
-/*
-int		main(void)//int argc, char **argv)
-{
-	//argc = 1;
-	long i = -6543444;
-	int a, b;
-
-	a = printf("Real: %+.10li hahaha", i);
-	b = ft_printf("Mine: %+.10li hahaha", i);
-	ft_putstr("\n^ len: ");
-	ft_putnbr(b);
-	ft_putstr("\nv len: ");
-	ft_putnbr(a);
-	ft_putchar('\n');
-	return (1);
-}
-*/
