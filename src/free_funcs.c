b052658305c837ad1fe939c85b343733d61d3979
Brendan Pierce <bpierce@e1z2r9p48.42.us.org>
2017-07-24 18:39:52 -0700

    Added new free funcs file that clears data when printf exits

---
+++ src/free_funcs.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_funcs.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpierce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/24 17:30:44 by bpierce           #+#    #+#             */
+/*   Updated: 2017/07/24 18:29:37 by bpierce          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+/*
+** s or ws point to same location in memory, so ft_strdel would work on either
+*/
+
+void	free_printf_datafmt(t_datafmt **fmt, int type)
+{
+	if (type == S_TYPE || type == WS_TYPE)
+		ft_strdel(&((*fmt)->s));
+	if (*fmt)
+	{
+		free(*fmt);
+		*fmt = NULL;
+	}
+}
+
+void	free_printf_percentid(t_percentid **pid)
+{
+	if (*pid)
+	{
+		free(*pid);
+		*pid = NULL;
+	}
+}
+
+void	free_all_the_things(t_printf **head)
+{
+	t_printf	*tmp;
+
+	if (*head)
+	{
+		while (*head)
+		{
+			if ((*head)->type == STRING_TYPE)
+				ft_strdel(&((*head)->str));
+			else
+			{
+				free_printf_datafmt(&((*head)->pid->fmt), (*head)->type);
+				free_printf_percentid(&((*head)->pid));
+				tmp = *head;
+				*head = (*head)->next;
+				free(tmp);
+			}
+		}
+	}
+}
